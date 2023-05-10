bc6b8e66b319934c221ce76dca78aa2a23bbb1b0
Ekaterina Churakova <katushachurakova@gmail.com>
2023-05-10 19:56:41 +0300

    "2"

--- tests/test_sum.py
+++ tests/test_sum.py
@@ -9,7 +9,16 @@ class TestPolyedr:
         assert Polyedr(f"data/test1.geom").sum_of_edges == 3
 
     def test_2(self):
-        assert Polyedr(f"data/test2.geom").sum_of_edges == approx(2*sqrt(6))
+        assert Polyedr(f"data/test2.geom").sum_of_edges == approx(2 * sqrt(6))
 
     def test_3(self):
         assert Polyedr(f"data/test3.geom").sum_of_edges == 0
+
+    def test_4(self):
+        assert Polyedr(f"data/test4.geom").sum_of_edges == approx((5 + sqrt(61)) * (1 / 2))
+
+    def test_5(self):
+        assert Polyedr(f"data/test5.geom").sum_of_edges == approx(sqrt(26) + sqrt(17))
+
+    def test_6(self):
+        assert Polyedr(f"data/test6.geom").sum_of_edges == approx(sqrt(17) / 2 + 2)
